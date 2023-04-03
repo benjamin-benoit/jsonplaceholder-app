e8a73aec31a40422d46839c74f8345bf56fe773b
Benjamin <benjaminbenoitfr@gmail.com>
2023-04-03 16:01:19 +0200

    :sparkles: Add react router and first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Posts from "./pages/Posts/Posts";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
